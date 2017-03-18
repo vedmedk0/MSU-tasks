1f1ae40e24245fe172564894521c10adb11827cb
vedmedk0 <mp.golubtcov@physics.msu.ru>
2017-03-18 23:36:12 +0300

    Add files via upload

--- mysed.py
+++ mysed.py
@@ -15,33 +15,35 @@ class WrongFileCommand(Exception):
 
 #right_input = True
 text=''
+print("Welcome to SED! If you want open file, print '/o %filename%' \n If you want write text manually, enter '/m %text%' \n If you want to see help enter /h \n if you want to exit enter /e : ")
 #print("Welcome to SED! If you want open file, print '/o %filename%', if you want write text manually, print '/m': ")
-while True: # стартовое меню закроется тогда, когда будет сделан выбор
     
-    command = input("Welcome to SED! If you want open file, print '/o %filename%', if you want write text manually, print '/m %text%': ")
-    if command[0:2] == '/o': #хотим открыть файл
+while True:
+    command = input()
+    if command == '/e': #хотим выйти
+        break
+    elif command[0:2] == '/o': #хотим открыть файл
         with open(command[3:len(command)]) as fin: #контекстный менедджер для безопасного открытия файла
             text = fin.read() #читаем текст из файла
-            break #выходим из стартового меню
     elif command[0:2] =='/m': #печатаем сами
         if len(command)<= 2:
-            print
-        text=command[3:len(command)]
-        break
-    elif command == '/e': #передумали что-то делать и выходим
-        sys.exit()
-    else:
-        print('Whoops! Wrong command.') #нет такой команды
-
-while True:
-    print('welcome! \n If you want to replace string1 with string2 enter /r/string1/string2 \n If you want to exit enter /e')
-    command = input()
-    if command == '/e':
-        break
-    elif command[0:2 ]== '/r':
+            print ('sorry! no text here')
+        else:
+            text=command[3:len(command)]
+    elif command[0:2 ]== '/r': #хотим заменить
         command=command[3:len(command)].split('/')
-        text.replace(command[0],command[1])
+        if len(command) == 1:   # если второй аргумент пустой
+            text=text.replace(command[0],'')
+        else:
+            text=text.replace(command[0],command[1])
         print (text)
+    elif command == '/p': #посмотреть редактируемый текст
+        print(text)
+    elif command[0:2] == '/s': #coхранить в файл
+        with open(command[3:len(command)], 'tw', encoding='utf-8') as f: #завести в открытой папке ненужный файл
+            f.write(text)
+    elif command[0:2] == '/h':
+        print('Replace/delete string1 with string2 : /r/string1/string2 \n Exit: /e \n Print: /p \n Save: /s %filename%')
     else:
         print('Whoops! Wrong command.')
 print ('bye')
